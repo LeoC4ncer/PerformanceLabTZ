66fd9ff99dca11095078cb3cdf9bba820e579052
LeoC4ncer <76601530+LeoC4ncer@users.noreply.github.com>
2023-10-31 13:05:55 +0400

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿uint amountOfNumbers;
+uint step;
+string answer = "";
+
+string line = Console.ReadLine().Trim();
+amountOfNumbers = Convert.ToUInt32(line.Substring(0, line.IndexOf(' ')).Trim());
+step = Convert.ToUInt32(line.Substring(line.IndexOf(' ')).Trim());
+
+uint currentNumber = 1;
+do
+{
+    answer += currentNumber.ToString();
+    currentNumber += (step - 1);
+    while (currentNumber > amountOfNumbers) 
+        currentNumber -= amountOfNumbers;
+} while (currentNumber != 1 );
+
+Console.WriteLine(answer);
+Console.ReadKey(true);
